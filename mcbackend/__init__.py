46d9e813e30aa987ad699e1565a5e30bd2898e48
Michael Osthege <michael.osthege@outlook.com>
2021-12-18 14:43:44 +0100

    Make `clickhouse-driver` an optional dependency

--- mcbackend/__init__.py
+++ mcbackend/__init__.py
@@ -1,7 +1,13 @@
 """
 A framework agnostic implementation for storage of MCMC draws.
 """
-from .clickhouse import ClickHouseBackend
 from .core import BackendBase, ChainMeta, RunMeta
+
+# Backends
+try:
+    from .clickhouse import ClickHouseBackend
+except ModuleNotFoundError:
+    pass
+
 
 __version__ = "0.1.0"
