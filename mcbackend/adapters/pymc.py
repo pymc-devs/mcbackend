dc8c3e3809a6f86a40008a2e701fa373d584d60d
Michael Osthege <michael.osthege@outlook.com>
2022-01-01 17:30:05 +0100

    Store dims and coords with the PyMC adapter
    Closes #2

--- mcbackend/adapters/pymc.py
+++ mcbackend/adapters/pymc.py
@@ -2,11 +2,13 @@
 This module implements an adapter to
 """
 import hagelkorn
+import numpy
 import pymc as pm
 
-from mcbackend.meta import Variable
+from mcbackend.meta import Coordinate, Variable
 
 from ..core import Backend, Chain, Run, RunMeta
+from ..npproto.utils import ndarray_from_numpy
 
 
 class TraceBackend(pm.backends.base.BaseTrace):
@@ -48,14 +50,24 @@ class TraceBackend(pm.backends.base.BaseTrace):
         free_rv_names = [rv.name for rv in self.model.free_RVs]
         if not self._run:
             variables = [
-                Variable(name, str(dtype), list(shape), (name in free_rv_names))
-                for name, dtype, shape in zip(
-                    self.varnames, self.var_dtypes.values(), self.var_shapes.values()
+                Variable(
+                    name,
+                    str(self.var_dtypes[name]),
+                    list(self.var_shapes[name]),
+                    (name in free_rv_names),
+                    dims=list(self.model.RV_dims[name]) if name in self.model.RV_dims else None,
                 )
+                for name in self.varnames
+            ]
+            coordinates = [
+                Coordinate(dname, ndarray_from_numpy(numpy.array(cvals)))
+                for dname, cvals in self.model.coords.items()
+                if cvals is not None
             ]
             rmeta = RunMeta(
                 self.run_id,
                 variables=variables,
+                coordinates=coordinates,
             )
             self._run = self._backend.init_run(rmeta)
         self._chain = self._run.init_chain(chain_number=chain)
--- mcbackend/test_adapter_pymc.py
+++ mcbackend/test_adapter_pymc.py
@@ -1,20 +1,33 @@
+import logging
+
 import clickhouse_driver
 import hagelkorn
+import numpy
 import pymc as pm
 import pytest
+
+from mcbackend.meta import RunMeta
+from mcbackend.npproto.utils import ndarray_to_numpy
 
 from .adapters.pymc import TraceBackend
 from .backends.clickhouse import ClickHouseBackend
 from .test_backend_clickhouse import HAS_REAL_DB
 
+_log = logging.getLogger(__file__)
+
 
 @pytest.fixture
 def simple_model():
-    with pm.Model() as pmodel:
-        pm.Normal("n")
-        pm.Uniform("u", size=3)
-        pm.Normal("2d", size=(5, 2))
-        pm.Bernoulli("b", p=0.5)
+    with pm.Model(
+        coords={
+            "condition": ["A", "B", "C"],
+        }
+    ) as pmodel:
+        time = pm.Data("time", numpy.arange(5), dims="time")
+        a = pm.Normal("scalar")
+        b = pm.Uniform("vector", dims="condition")
+        pm.Deterministic("matrix", a + b[:, None] * time[None, :], dims=("condition", "time"))
+        pm.Bernoulli("integer", p=0.5)
     return pmodel
 
 
@@ -42,6 +55,16 @@ class TestPyMCAdapter:
     def test_cores(self, simple_model, cores):
         backend = ClickHouseBackend(self._client)
 
+        # To extract the run meta that the adapter passes to the backend:
+        args = []
+        original = backend.init_run
+
+        def wrapper(meta: RunMeta):
+            args.append(meta)
+            return original(meta)
+
+        backend.init_run = wrapper
+
         with simple_model:
             trace = TraceBackend(backend)
             idata = pm.sample(
@@ -54,8 +77,24 @@ class TestPyMCAdapter:
                 discard_tuned_samples=False,
                 compute_convergence_checks=False,
             )
+        if not len(args) == 1:
+            _log.warning("Run was initialized multiple times.")
+        rmeta = args[0]
+
+        # Chain lenghts after conversion
         assert idata.posterior.dims["chain"] == 2
         assert idata.posterior.dims["draw"] == 5
         assert idata.warmup_posterior.dims["chain"] == 2
         assert idata.warmup_posterior.dims["draw"] == 3
+
+        # Tracking of named variable dimensions
+        vars = {var.name: var for var in rmeta.variables}
+        assert vars["vector"].dims == ["condition"]
+        assert vars["matrix"].dims == ["condition", "time"]
+
+        # Tracking of coordinates
+        coords = {coord.name: coord for coord in rmeta.coordinates}
+        assert (
+            tuple(ndarray_to_numpy(coords["condition"].values)) == simple_model.coords["condition"]
+        )
         pass
